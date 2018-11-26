8ff467be8f4c5d544321386f31b3c47b32195ff8
Sary Elmelegy <arch.selmelegy@gmail.com>
2018-11-27 00:56:26 +0200

    restructure and commenting cards

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,32 @@ $(document).ready(function(){
     url: 'https://randomuser.me/api/?results=12',
     dataType: 'json',
     success: function(data) {
-      // console.log(data.results[0].name.first);
-      // console.log(data.results);
+
+      // Getting and displaying 12 random users
       $.each(data.results, function(index, item){
-        // console.log(item);
-        // console.log(item.picture);
         let card = '<div class="card">';
-        const userThumbnail = item.picture.large;
-        const first = item.name.first;
-        const last = item.name.last;
-        const email = item.email;
-        const city = item.location.city;
-        const state = item.location.state;
+        const info = {
+          userThumbnail: item.picture.large,
+          first: item.name.first,
+          last: item.name.last,
+          email: item.email,
+          city: item.location.city,
+          state: item.location.state
+        }
         card += '<div class="card-img-container">';
-        card += `<img class="card-img" src="${userThumbnail}" alt="profile picture">`;
+        card += `<img class="card-img" src="${info.userThumbnail}" alt="profile picture">`;
         card += '</div><div class="card-info-container">';
         card += `
-          <h3 id="name" class="card-name cap">${first} ${last}</h3>
-          <p class="card-text">${email}</p>
-          <p class="card-text cap">${city}, ${state}</p>
+          <h3 id="name" class="card-name cap">${info.first} ${info.last}</h3>
+          <p class="card-text">${info.email}</p>
+          <p class="card-text cap">${info.city}, ${info.state}</p>
         `;
         card += '</div></div>';
         $('#gallery').append(card);
-      })
-    }
-  });
+      }) // end each
+
+
+
+    } // end success
+  }); // end ajax
 }) // end ready
